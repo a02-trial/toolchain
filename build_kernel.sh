a8441f72a75bc33c50a0deaaf5bf195fee7390e0
a02-trial <cipin23@gmail.com>
2025-12-22 02:50:01 +0700

    Update build_kernel.sh

--- build_kernel.sh
+++ build_kernel.sh
@@ -13,17 +13,24 @@ chmod +x -R .
 # toolchain preparation
 export PATH="${PWD}/toolchain/clang/bin:${PWD}/toolchain/gcc/bin:${PATH}"
 
+export ARCH=arm
+export CC=clang
+export HOSTCC=clang
+export CROSS_COMPILE=arm-linux-androideabi-
 export KCFLAGS=-w
 export CONFIG_SECTION_MISMATCH_WARN_ONLY=y
-export CFLAGS_WARN=-Wunused-but-set-variable
-make O=out KCFLAGS=-w CONFIG_SECTION_MISMATCH_WARN_ONLY=y CFLAGS_WARN=-Wunused-but-set-variable ARCH=arm CC=clang HOSTCC=clang CROSS_COMPILE=arm-linux-androideabi- clean
-make O=out KCFLAGS=-w CONFIG_SECTION_MISMATCH_WARN_ONLY=y CFLAGS_WARN=-Wunused-but-set-variable ARCH=arm CC=clang HOSTCC=clang CROSS_COMPILE=arm-linux-androideabi- mrproper
-make O=out KCFLAGS=-w CONFIG_SECTION_MISMATCH_WARN_ONLY=y CFLAGS_WARN=-Wunused-but-set-variable ARCH=arm CC=clang HOSTCC=clang CROSS_COMPILE=arm-linux-androideabi- a02_defconfig
+export CFLAGS_WARN=-Wunused-but-set-variable \
+                   -Wunused-command-line-argument
+export xxx="KCFLAGS=-w CONFIG_SECTION_MISMATCH_WARN_ONLY=y CFLAGS_WARN=-Wunused-but-set-variable \
+            CFLAGS_WARN=-Wunused-command-line-argument ARCH=arm CROSS_COMPILE=arm-linux-androideabi- CC=clang HOSTCC=clang"
+make O=out $xxx clean
+make O=out $xxx mrproper
+make O=out $xxx a02_defconfig
 # make O=out KCFLAGS=-w CONFIG_SECTION_MISMATCH_WARN_ONLY=y CFLAGS_WARN=-Wunused-but-set-variable ARCH=arm CC=clang HOSTCC=clang CROSS_COMPILE=arm-linux-androideabi- dtbs
-make O=out KCFLAGS=-w CONFIG_SECTION_MISMATCH_WARN_ONLY=y CFLAGS_WARN=-Wunused-but-set-variable ARCH=arm CC=clang HOSTCC=clang CROSS_COMPILE=arm-linux-androideabi- -j16 modules
+make O=out $xxx -j16 modules
 # make O=out KCFLAGS=-w CONFIG_SECTION_MISMATCH_WARN_ONLY=y CFLAGS_WARN=-Wunused-but-set-variable ARCH=arm CC=clang HOSTCC=clang CROSS_COMPILE=arm-linux-androideabi- modules_install
 
-dtc -I dts -O dtb -o mt6739.dtbo mt6739.dts
+# dtc -I dts -O dtb -o mt6739.dtbo mt6739.dts
 
 # cp out/arch/arm/boot/zImage ${PWD}/zImage
 # mv zImage boot.img-kernel
