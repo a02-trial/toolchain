191d482554e0d81b00b29c9b91a2aa5cdfcb933d
a02-trial <cipin23@gmail.com>
2025-12-22 12:04:55 +0700

    Update build_kernel.sh

--- build_kernel.sh
+++ build_kernel.sh
@@ -23,18 +23,16 @@ export xxx="KCFLAGS=-w CONFIG_SECTION_MISMATCH_WARN_ONLY=y CFLAGS_WARN=-Wunused-
 # make O=out $xxx clean
 # make O=out $xxx mrproper
 make O=out $xxx a02_defconfig
-make O=out $xxx dtbs
-# make O=out $xxx -j16 modules
+make O=out $xxx -j16 modules dtbs
 # make O=out $xxx modules_install
 
-cd ${GITHUB_WORKSPACE}
-sudo apt-get install -y device-tree-compiler
-git clone https://git.kernel.org/pub/scm/utils/dtc/dtc.git
-cd dtc
-export PATH="${PWD}:${PATH}"
-make
+# sudo apt-get install -y device-tree-compiler
+# git clone https://git.kernel.org/pub/scm/utils/dtc/dtc.git
+# cd dtc
+# export PATH="${PWD}/dtc:${PATH}"
+# make
 # cd ${GITHUB_WORKSPACE}/arch/arm/boot/dts
-cpp -nostdinc -I ${GITHUB_WORKSPACE}/arch/arm/boot/dts/samsung/a02/a02_eur_open_w00_r06.dts -x assembler-with-cpp a02_eur_open_w00_r06.dts | dtc -O dtb -o a02_eur_open_w00_r06.dtb
+# cpp -nostdinc -I ${GITHUB_WORKSPACE}/arch/arm/boot/dts/samsung/a02 -x assembler-with-cpp a02_eur_open_w00_r06.dts | dtc -O dtb -o a02_eur_open_w00_r06.dtb
 
 # USE CPP TO COMBINE DTS AND DTSI FILES INTO DTS SUITABLE FOR DTC COMMAND
 # cpp -DLINUX_VERSION -nostdinc -I ${GITHUB_WORKSPACE}/arch/arm/boot/dts/bat_setting -I ${GITHUB_WORKSPACE}/arch/arm/boot/dts/k39tv1_bsp_titan_hamster -I ${GITHUB_WORKSPACE}/arch/arm/boot/dts/samsung -I ${GITHUB_WORKSPACE}/arch/arm/boot/dts/samsung/a02 -undef -x assembler-with-cpp a02_eur_open_w00_r06.dts
