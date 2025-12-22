d272855bb9f974efbe9500e6690f5348f604879d
a02-trial <cipin23@gmail.com>
2025-12-22 11:44:38 +0700

    Update build_kernel.sh

--- build_kernel.sh
+++ build_kernel.sh
@@ -20,8 +20,8 @@ export CONFIG_SECTION_MISMATCH_WARN_ONLY=y
 export CFLAGS_WARN=-Wunused-but-set-variable
 export xxx="KCFLAGS=-w CONFIG_SECTION_MISMATCH_WARN_ONLY=y CFLAGS_WARN=-Wunused-but-set-variable \
             ARCH=arm CROSS_COMPILE=arm-linux-androideabi- CC=clang HOSTCC=clang"
-make O=out $xxx clean
-make O=out $xxx mrproper
+# make O=out $xxx clean
+# make O=out $xxx mrproper
 make O=out $xxx a02_defconfig
 make O=out $xxx dtbs
 # make O=out $xxx -j16 modules
@@ -38,7 +38,7 @@ make
 
 # USE CPP TO COMBINE DTS AND DTSI FILES INTO DTS SUITABLE FOR DTC COMMAND
 # cpp -DLINUX_VERSION -nostdinc -I ${GITHUB_WORKSPACE}/arch/arm/boot/dts/bat_setting -I ${GITHUB_WORKSPACE}/arch/arm/boot/dts/k39tv1_bsp_titan_hamster -I ${GITHUB_WORKSPACE}/arch/arm/boot/dts/samsung -I ${GITHUB_WORKSPACE}/arch/arm/boot/dts/samsung/a02 -undef -x assembler-with-cpp a02_eur_open_w00_r06.dts
-cpp -DLINUX_VERSION -nostdinc -I ${GITHUB_WORKSPACE}/arch/arm/boot/dts/samsung/a02 -undef -x assembler-with-cpp a02_eur_open_w00_r06.dts
+# cpp -DLINUX_VERSION -nostdinc -I ${GITHUB_WORKSPACE}/arch/arm/boot/dts/samsung/a02 -undef -x assembler-with-cpp a02_eur_open_w00_r06.dts
 # DTC COMMAND TO PRODUCE DTB FROM DTS
 dtc -I dts -O dtb -o a02_eur_open_w00_r06.dtb -i ${GITHUB_WORKSPACE}/arch/arm/boot/dts/samsung/a02/a02_eur_open_w00_r06.dts
 
