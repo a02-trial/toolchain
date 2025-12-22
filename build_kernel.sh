41882254dd24bc4f319a48e7d29abd4112a16e4e
a02-trial <cipin23@gmail.com>
2025-12-22 10:36:54 +0700

    Update build_kernel.sh

--- build_kernel.sh
+++ build_kernel.sh
@@ -27,8 +27,8 @@ make O=out $xxx dtbs
 # make O=out $xxx -j16 modules
 # make O=out $xxx modules_install
 
-cd out/arch/arm/boot/dts
-dtc -I dts -O dtb -o mt6739.dtbo mt6739.dts
+# cd out/arch/arm/boot/dts
+dtc -I dts -O dtb -o mt6739.dtb mt6739.dts
 
 # cp out/arch/arm/boot/zImage ${PWD}/zImage
 # mv zImage boot.img-kernel
