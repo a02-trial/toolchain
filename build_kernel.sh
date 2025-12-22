661cfc77ab90525e6b9ce25c04fe1e12866d7246
a02-trial <cipin23@gmail.com>
2025-12-22 10:39:25 +0700

    Update build_kernel.sh

--- build_kernel.sh
+++ build_kernel.sh
@@ -26,7 +26,7 @@ make O=out $xxx a02_defconfig
 make O=out $xxx dtbs
 # make O=out $xxx -j16 modules
 # make O=out $xxx modules_install
-
+sudo apt-get install -y device-tree-compiler
 # cd out/arch/arm/boot/dts
 dtc -I dts -O dtb -o mt6739.dtb mt6739.dts
 
