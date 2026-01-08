409564fc0fd029429b64e5376d1d735ec6bec95d
a02-trial <cipin23@gmail.com>
2026-01-08 15:37:49 +0700

    Update ba.sh

--- ba.sh
+++ ba.sh
@@ -1,22 +1,13 @@
 #!/bin/sh
 
-sudo apt-get update && sudo apt-get upgrade -y
 set -e -x
 cd ${GITHUB_WORKSPACE}
-ls
-chmod +x -R .
 
-# custom toolchain preparation
-# export PATH="${PWD}/toolchain2/clang/bin:${PWD}/toolchain2/gcc/bin:${PATH}"
+# Setup make Command
+make_fun() {
+	make O=out KCFLAGS=-w CONFIG_SECTION_MISMATCH_WARN_ONLY=y CFLAGS_WARN=-Wunused-but-set-variable \
+		ARCH=arm CROSS_COMPILE=arm-linux-androideabi- CC=clang HOSTCC=clang "$@"
+}
 
-# toolchain preparation
-# export PATH="${PWD}/toolchain/clang/bin:${PWD}/toolchain/gcc/bin:${PATH}"
-
-export ARCH=arm
-export CC=clang
-export HOSTCC=clang
-export CROSS_COMPILE=arm-linux-androideabi-
-export KCFLAGS=-w
-export CONFIG_SECTION_MISMATCH_WARN_ONLY=y
-export CFLAGS_WARN=-Wunused-but-set-variable
-export xxx="ARCH=arm CC=clang CROSS_COMPILE=arm-linux-androideabi- HOSTCC=clang"
+# make_fun a02_defconfig
+make_fun -j"$(nproc --all)" vmlinux
